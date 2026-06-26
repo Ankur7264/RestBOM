d636fcdc2f8426711e0b2985df7f074bc00d5cef
ANKUR <139110035+Ankur7264@users.noreply.github.com>
2026-06-26 17:41:46 +0530

    Add HttpUtils.h with cURL callback function

---
+++ HttpUtils.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+// Callback function required by cURL to capture HTTP responses
+inline size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
+}
